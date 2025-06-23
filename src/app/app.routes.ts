cd8c40e680930e0e497ff07972c9eed9646ef167
LosCantabrones <loscantabrones2024@gmail.com>
2025-06-23 22:27:56 +0200

    primera base

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InfoComponent } from './pages/info/info.component';
+import { PremiosComponent } from './pages/premios/premios.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'premios', component: PremiosComponent },
+  { path: '**', component: NotFoundComponent }
+];
---
+++ src/app/pages/premios/premios.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-premios',
+  standalone: true,
+  imports: [],
+  templateUrl: './premios.component.html',
+  styleUrl: './premios.component.scss'
+})
+
+export class PremiosComponent {
+
+}
